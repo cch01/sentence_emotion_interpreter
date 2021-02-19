6e331ced1f394c6e17d4058d7c7ab11b0181d4c7
cch01 <chanchiho01@gmail.com>
2021-02-19 18:15:41 +0800

    TODO: add test data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import nltk
 from nltk.tokenize import word_tokenize, WordPunctTokenizer
 from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-
+from nltk import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
 
 tokenized_sent = []
 
@@ -11,7 +13,6 @@ with open ('./data/train.txt') as td:
   stop_words = set(stopwords.words("english"))
   lem = WordNetLemmatizer()
 
-  # print(doc)
   for sent in doc:
     sent_tuple = sent.split(';')
     tokenized_words = [word.lower() for word in word_tokenize(sent_tuple[0])]
@@ -21,8 +22,20 @@ with open ('./data/train.txt') as td:
 
 td.close()
 
-print(tokenized_sent[:50])
-for tuple in tokenized_sent:
-  print(tuple[1])
+# print(tokenized_sent[:50])
 emotions = ['anger', 'fear', 'joy', 'love', 'sadness', 'surprise']
+
+tf=TfidfVectorizer()
+text_tf= tf.fit_transform([' '.join(tuple[0]) for tuple in tokenized_sent])
+
+X_train, X_test, y_train, y_test = train_test_split(text_tf, [tuple[1] for tuple in tokenized_sent], test_size=0.3)
+
+
+# Model Generation Using Multinomial Naive Bayes
+clf = MultinomialNB().fit(X_train, y_train)
+predicted= clf.predict(X_test)
+print('predicted', predicted[:200])
+print('y_test', y_test[:200])
+print("MultinomialNB Accuracy:",metrics.accuracy_score(y_test, predicted))
+# print(metrics.)
 
